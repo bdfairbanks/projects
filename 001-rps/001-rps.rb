35608e8c7749471849db555d71e704ba6b5de973
Ben Fairbanks <bdfairbanks3@gmail.com>
2017-02-07 20:19:24 -0600

    work done on 002

--- 001-rps/001-rps.rb
+++ 001-rps/001-rps.rb
@@ -20,6 +20,7 @@ class Handstyle
  			puts "Pick rock, paper, or scissors, dummy."
  		end
  	end
+
  	def return
  		return @choice
 	end
@@ -44,27 +45,19 @@ class Tally
 
 	def remember_wins
 		if @counter_p1 == 3 && @win_counter_p1 <3
-
 			@win_counter_p1 += 1
-
-			@counter_p1 = 0
-
-			@counter_p2 = 0
-
-			return puts "p1 win one!(round)"
-
+			puts "p1 win one!(round)"
 		elsif @counter_p2 == 3 && @win_counter_p2 <3
-
 			@win_counter_p2 += 1
-
-			@counter_p1 =0
-
-			@counter_p2 =0
-
-			return puts "p2 wins sometimes too!"
+			puts "p2 wins sometimes too!"
 		end	
+		reset_count
 	end
 
+	def reset_count
+		@counter_p1 = 0
+		@counter_p2 = 0
+	end
 
 	def return_counter_p1
 		return @counter_p1
@@ -87,47 +80,29 @@ end
 #eventually there will be one p_input, i swear...
 
 def get_input_p1()
-	
 	p1_choice =Handstyle.new
-
 	while p1_choice.return == nil 
-
 		p1_choice.holder
-
 		p1_choice.ask_correctly
-
 		p1_choice.weed_out
-
 	end	
-	
 	puts "The future winner picked " +p1_choice.return
-
-return p1_choice.return
-
+	return p1_choice.return
 end
 
 
 def get_input_p2()
-	
 	p2_choice =Handstyle.new
-	
 	while p2_choice.return == nil 
-	
-		p2_choice =Handstyle.new
-
 		p2_choice.holder
-
 		p2_choice.ask_correctly
-
 		p2_choice.weed_out
-
 	end
-
 	puts "The loathesome p2 picked " +p2_choice.return
-
-return p2_choice.return
-
+	return p2_choice.return
 end
+
+# Consider changing return from two values to a single value -- either 0, 1, or 2 -- and changing other functions accordingly.
 #the duel mechanism is the main engine of this peice.
 def duel(choice1, choice2)
 	if choice1 == "rock" && choice2 == "scissors"||choice1 == "paper" && choice2 == "rock"||choice1 == "scissors" && choice2 == "paper"
@@ -140,24 +115,18 @@ def duel(choice1, choice2)
 		 points1 = 0
 		 points2 = 1
 	end
-
 	return points1, points2
 end
 
 def judge(result)
-		if result == [1,0]
-
-			puts "P1 wins this round"
-
-		elsif 
-
-			result==[0,1]
-
-			puts "P2 wins, somehow."
-
-		else puts "A tie? Disgusting."
-
-		end
+	if result == [1,0]
+		puts "P1 wins this round"
+	elsif 
+		result==[0,1]
+		puts "P2 wins, somehow."
+	else 
+		puts "A tie? Disgusting."
+	end
 end
 #begin Program
 
@@ -170,27 +139,17 @@ while tally.return_win_counter_p1 <3 && tally.return_win_counter_p2 <3
 
 #another messy loop, this one keeping track of the game by game score.
 	while tally.return_counter_p1 <3 && tally.return_counter_p2 <3
-
 		p1_choice = get_input_p1()
-
 		p2_choice = get_input_p2()
-
-
 		thats = duel(p1_choice, p2_choice)
-
 		judge(thats)
-
 		tally.add_to_counter_p1(thats.fetch(0))
-		
 		tally.add_to_counter2(thats.fetch(1))
-		
 		tally.remember_wins
-
 	end
 
 	if tally.return_win_counter_p1 == 3
-	puts "Finish them, player 1!!!" 	
-	
+		puts "Finish them, player 1!!!" 	
 	elsif tally.return_win_counter_p2 == 3
 		puts "Good game, p2, but p1 still wins somehow!!!"
 	end
--- 002-fb/scripts.js
+++ 002-fb/scripts.js
@@ -1,12 +1,23 @@
-window.addEventListener("load", allStuff);
+window.addEventListener("load", repliesFunctionality);
 
-function allStuff(){
+function repliesFunctionality(){
 	function attention(){
-	 debugger
-		 sublime= this.firstChild
-		sublime.style.display = "block";
-		
+		that = this.parentElement
+		theOther = that.nextElementSibling 
+		if (theOther.style.display == "none"){
+			theOther.style.display = "block"
+			var gratify = document.getElementsByClassName("reply");
+			for (var i=0; i<gratify.length; i++)
+			gratify[i].addEventListener("click", attention)
 		}
+		else if (theOther.style.display == "block"){
+			theOther.style.display = "none"
+
+			var gratify = document.getElementsByClassName("reply");
+			for (var i=0; i<gratify.length; i++)
+			gratify[i].addEventListener("click", attention)
+		}
+	}
 	
 
 	var gratify = document.getElementsByClassName("reply");
@@ -17,10 +28,21 @@ function allStuff(){
 
 window.addEventListener("load", likes);
 	function likes(){
-		console.log(this.lastChild)
+		function test(){
 
+		likeField =this.nextElementSibling.nextElementSibling.innerHTML;
+		numString= likeField[0];
+		likeField =likeField.substring(1)
+		numString =Number(numString);
+			numString = numString+1;
+			finished = numString.toString().concat(likeField);
+			this.nextElementSibling.nextElementSibling.innerHTML = finished
 
-	var pray = document.getElementsByClassName("action--like");
+		var pray = document.getElementsByClassName("prayer");
+		for (var i=0; i<pray.length; i++)
+		pray[i].addEventListener("click", test)
+	}
+	var pray = document.getElementsByClassName("prayer");
 	for (var i=0; i<pray.length; i++)
-		pray[i].addEventListener("click", likes)
+		pray[i].addEventListener("click", test)
 }
